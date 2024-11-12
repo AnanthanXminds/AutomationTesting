589612b655127aed969bffadc54273d48542b5ef
AnanthanXminds <ananthakrishnan.vs@xminds.com>
2024-11-12 14:30:12 +0530

    Duplicate words

--- LearnAutomation/src/com/test/DuplicateWord.java
+++ LearnAutomation/src/com/test/DuplicateWord.java
@@ -1,5 +1,8 @@
 package com.test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DuplicateWord {
@@ -8,17 +11,23 @@ public class DuplicateWord {
     public void findDuplicate() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your string sequence");
-        yourSentence = sc.next();
-        System.out.println(yourSentence);
+        yourSentence = sc.nextLine();
+        List<String> duplicateWords = new ArrayList<>();
         String [] word = yourSentence.split(" ");
         for (int i=0;i<word.length;i++){
             for (int j=i+1;j< word.length;j++){
-                if (word[i].equalsIgnoreCase(word[j])){
-                    System.out.println(word[i]+" is repeating");
+                if (word[i].equalsIgnoreCase(word[j]) && !duplicateWords.contains(word[i])){
+                    duplicateWords.add(word[i]);
                 }
             }
         }
+        System.out.println("Repeating word are : ");
+        duplicateWords.forEach(System.out::println);
 
 
+    }
+    public static void main(String args []){
+        DuplicateWord duplicateWord = new DuplicateWord();
+        duplicateWord.findDuplicate();
     }
 }
