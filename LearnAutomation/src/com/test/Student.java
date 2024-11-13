f930e98d2c0f96d67c9cfb350c9d0bbeb5cc54f1
AnanthanXminds <ananthakrishnan.vs@xminds.com>
2024-11-13 14:09:29 +0530

    Student class

--- LearnAutomation/src/com/test/Student.java
+++ LearnAutomation/src/com/test/Student.java
@@ -1,6 +1,7 @@
 package com.test;
 
 public class Student {
+    private Integer id;
     private  String name;
     private Integer age;
     public Integer getAge(){
@@ -14,16 +15,25 @@ public class Student {
     public void setName(String name){
         this.name=name;
     }
+    public void setId(Integer id){
+        this.id = id;
+    }
     public String getName(){
         return this.name;
     }
-    public Student(String name, Integer age) {
+    public Student(String name, Integer age, Integer id) {
         this.name = name;
         this.age = age;
+        this.id = id;
     }
     public static void main(String args[]){
         Student student1 = new Student();
-        student1.setName("Jenkins");
-        student1.setAge(33);
+        student1.name = "Ananthan";
+        student1.id = 1;
+        student1.age = 27;
+        Student student2 = new Student();
+        student2.setName("Jenkins");
+        student2.setAge(33);
+        student2.setId(2);
     }
 }
